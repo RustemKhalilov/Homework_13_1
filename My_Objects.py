d64430f45968a6205068cf4c23929ed2f106aa9a
Rustem Khalilov <rvkhalilov@gmail.com>
2024-02-24 18:02:11 +0500

    rev3

--- My_Objects.py
+++ My_Objects.py
@@ -1,26 +1,27 @@
 class Category:
     category_count = 0
     product_count = 0
-    product_list = {}
 
-    def __init__(self, name: str, description: str, number_of_categories=-2115,
+    def __init__(self, name: str, description: str, products, number_of_categories=-2115,
                  total_number_of_unique_products=-2116):
         self.name = name
         self.description = description
         self.number_of_categories = number_of_categories
         self.total_number_of_unique_product = total_number_of_unique_products
+        self.products = products
+        Category.category_count += 1
+        Category.product_count += len(self.products)
 
     def __repr__(self):
         return f'{self.name} \n  {self.description} \n {self.number_of_categories} \n {self.total_number_of_unique_product}'
 
 
 class Product:
-    def __init__(self, name: str, description: str, price: float, quantity: float, category_name: str):
+    def __init__(self, name: str, description: str, price: float, quantity: float):
         self.name = name
         self.description = description
         self.price = price
         self.quantity = quantity
-        self.category_name = category_name
 
     def __repr__(self):
-        return f'{self.name} \n  {self.description} \n {self.price} \n {self.quantity} \n {self.category_name}'
+        return f'{self.name} \n  {self.description} \n {self.price} \n {self.quantity}'
--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from My_Objects import Category
 from My_Objects import Product
-from My_func import load_json_from_file, counter_category, counter_product
+from My_func import load_json_from_file
 
 if __name__ == '__main__':
-    # Пример создания объекта
-    category_1 = Category("Карандаши", "Канцтовары", 10, 2)
-    Product_1 = Product("Карандаш простой", "Канцтовары", 35.43, 523, "Карандаши")
-
     # Чтение данных из json
     path1 = "operations"
     file_name = "products.json"
@@ -15,31 +11,14 @@ if __name__ == '__main__':
     # Создаем список из объектов и заполняем параметры для объекта
     Shop_category = []
     Products = []
-    Products_for_category = {}
     for item in dictionary_data:
-        Category_prod = Category(item['name'], item['description'])
-        Shop_category.append(Category_prod)
         Product_list = []
         for item2 in item['products']:
-            Product_prod = Product(item2['name'], item2['description'], item2['price'], item2['quantity'], item['name'])
+            # Формируем словарь продуктов
+            Product_list.append({item2['name']: item2['quantity']})
+            # Создаем объект продукт
+            Product_prod = Product(item2['name'], item2['description'], item2['price'], item2['quantity'])
             Products.append(Product_prod)
-            Product_list.append([item2['name'], item2['quantity']])
-        # Формируем словарь продуктов
-        Products_for_category[item['name']] = Product_list
-    # Расчет количества категорий
-    Category_cont = counter_category(Shop_category)
-    # Запись количества категорий в объект категория
-    for item in Shop_category:
-        # Расчитали количество категорий и записали их в объекты
-        item.category_count = Category_cont
-        # Записали список продуктов в каждый объект
-        item.product_list = Products_for_category[item.name]
-    # Поиск и запись количества продуктов в объект категория
-    for item in Shop_category:
-        summ_prod = 0
-        for item2 in Products:
-            if item2.category_name == item.name:
-                summ_prod += item2.quantity
-        item.product_count = summ_prod
-
-
+        # Создаем список объектов Product
+        Category_prod = Category(item['name'], item['description'], Product_list)
+        Shop_category.append(Category_prod)
